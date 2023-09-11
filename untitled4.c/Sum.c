e1c2a9d2c44937437bbabeeb81930380e0f5f6df
tuawahn18 <Tuaasnanh188@gmail.com>
2023-09-11 19:35:51 +0700

    Demo1

---
+++ untitled4.c/Sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int a, b, sum;
+    printf(" a=n");
+    scanf("%d", &a);
+    printf(" b=m");
+    scanf("%d", &b);
+    sum = a + b;
+    printf("the sum is: %d", sum);
+    return 0;
+
+}
